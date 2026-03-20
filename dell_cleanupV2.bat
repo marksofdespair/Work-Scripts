a7864337032f030d1c77e6ae66dce24cf1a4c820
Dia <142447820+marksofdespair@users.noreply.github.com>
2026-03-20 08:12:15 -0500

    Update dell_cleanupV2.bat
    Should fix stubborn working directory issue causing it to not uninstall dell optimizer

--- dell_cleanupV2.bat
+++ dell_cleanupV2.bat
@@ -3,12 +3,11 @@
 :: Check for admin rights so it still runs as admin (?)
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo Requesting administrative privileges...
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
 
 :: Run PowerShell script with execution policy bypass because dellware moment
-powershell.exe -ExecutionPolicy Bypass -File "C:\Temp\dell_cleanupV2.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Temp\dell_cleanupV2.ps1"
 
 pause
